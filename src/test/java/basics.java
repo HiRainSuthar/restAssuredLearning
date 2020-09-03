a3a0af24337ca56765447d68c13c1542db3adca4
devhiren13042019 <montallis@gmail.com>
2020-09-03 16:09:27 +0530

    extracting json response and getting value of key and gitignore removed from tracking

--- src/test/java/basics.java
+++ src/test/java/basics.java
@@ -1,4 +1,5 @@
 import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
 
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
@@ -10,10 +11,16 @@ public class basics {
         RestAssured.baseURI = "https://rahulshettyacademy.com";
         //Given block , queryParam if any , header , body , when, then , log all , status code validation,
         //status parameter validation and header validation
-        given().log().all().queryParam("Key", "qaclick123").header("Content-type", "application/json")
+        String response = given().log().all().queryParam("Key", "qaclick123").header("Content-type", "application/json")
                 .body(requestBody.returnRequestBody()).
                 when().post("/maps/api/place/add/json").
                 then().log().all().assertThat().statusCode(200).body("status", equalTo("OK"))
-                .header("server", "Apache/2.4.18 (Ubuntu)");
+                .header("server", "Apache/2.4.18 (Ubuntu)").extract().response().asString();
+        //printing extracted response
+//        System.out.println("response is "+ response);
+        //Using jsonpath parsing json
+        JsonPath jpath  = new JsonPath(response);
+        String placeId = jpath.getString("place_id");
+        System.out.println("value of place_Id is "+ placeId);
     }
 }
